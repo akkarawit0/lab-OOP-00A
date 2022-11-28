3cdfa29aa748716a7fc95c5b51d197743ce6cbc4
akkarawit0 <119285508+akkarawit0@users.noreply.github.com>
2022-11-28 15:37:47 +0700

    summary by akkarawit

---
+++ lab OOP 00A/lab OOP 00A/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Name : Akkarawit Pobwongsa");
+Console.WriteLine("ID : 653450108-3");
+Console.WriteLine("E-mail : Akkarawit.p@kkumail.com");
